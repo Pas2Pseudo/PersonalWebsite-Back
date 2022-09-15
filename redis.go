b5a419716dc9b472e7a7a1e7b04ae3266a54b104
Pas2Pseudo <80533655+Pas2Pseudo@users.noreply.github.com>
2022-09-15 08:25:53 +0200

    Add error acceptation RedisManager

--- redis.go
+++ redis.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"github.com/go-redis/redis/v9"
+	"log"
 	"strconv"
 )
 
@@ -13,16 +14,31 @@ type RedisManager struct {
 }
 
 func (m RedisManager) set(key string, value string) {
-	m.client.Set(ctx, key, value, 0)
+	err := m.client.Set(ctx, key, value, 0).Err()
+
+	if err != nil {
+		log.Fatalf("Redis error: %v", err.Error())
+		return
+	}
 }
 
 func (m RedisManager) get(key string) string {
-	val, _ := m.client.Get(ctx, key).Result()
+	val, err := m.client.Get(ctx, key).Result()
+
+	if err != nil {
+		log.Fatalf("Redis error: %v", err.Error())
+		return ""
+	}
 	return val
 }
 
 func (m RedisManager) del(key string) {
-	m.client.Del(ctx, key)
+	err := m.client.Del(ctx, key).Err()
+
+	if err != nil {
+		log.Fatalf("Redis error: %v", err.Error())
+		return
+	}
 }
 
 func newRedisManager(addr string, port int, password string, db int) *RedisManager {
